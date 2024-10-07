48bf30b2e781dcf75c128a1daa706fc5e8cde138
Loacir <loacirr@gmail.com>
2024-10-07 12:49:55 -0300

    Mudança nas paginas Home e Comunidade, dixando duas fotos em telas grandes e uma em telas Pequenas

--- src/Componentes/Comunidade/comunidade.jsx
+++ src/Componentes/Comunidade/comunidade.jsx
@@ -20,6 +20,18 @@ const properties = [
     name: "Fotos da comunidade ",
     desc: "orem ipsum dolor sit amet . Os operadores gráficos e tipográficos sabem disso bem, na realidade, todas as profissões que lidam com o universo da comunicação têm um relacionamento estável com essas palavras, mas o que é? Lorem ipsum é um texto fofo sem qualquer sentido. É uma seqüência de palavras latinas que, como estão posicionadas, não formem frases com um sentido com",
   },
+{
+  id: 3,
+  imagem: image1,
+  name: "Fotos da comunidade ",
+  desc: "orem ipsum dolor sit amet . Os operadores gráficos e tipográficos sabem disso bem, na realidade, todas as profissões que lidam com o universo da comunicação têm um relacionamento estável com essas palavras, mas o que é? Lorem ipsum é um texto fofo sem qualquer sentido. É uma seqüência de palavras latinas que, como estão posicionadas, não formem frases com um sentido completo, mas dão vida a um texto de teste útil para preencher",
+},
+{
+  id: 4,
+  imagem: image1,
+  name: "Fotos da comunidade ",
+  desc: "orem ipsum dolor sit amet . Os operadores gráficos e tipográficos sabem disso bem, na realidade, todas as profissões que lidam com o universo da comunicação têm um relacionamento estável com essas palavras, mas o que é? Lorem ipsum é um texto fofo sem qualquer sentido. É uma seqüência de palavras latinas que, como estão posicionadas, não formem frases com um sentido com",
+},
 ];
 
 function Comunidade() {
