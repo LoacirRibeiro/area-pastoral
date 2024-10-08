4082090ea4037e92f0b714b6b625c44efc338898
Loacir <loacirr@gmail.com>
2024-10-08 18:01:05 -0300

    Feito a atualização da pagina Contatos, Adicionado icons e atualizado css

--- src/Componentes/Footer/footer.jsx
+++ src/Componentes/Footer/footer.jsx
@@ -1,14 +1,9 @@
 import React, { useEffect } from "react";
 import "./footer.css";
-
 import img from "../../Assets/imgFooter.jpeg";
-
-// importação de icones
-
 import { ImFacebook } from "react-icons/im";
 import { AiFillInstagram } from "react-icons/ai";
 import { IoLogoWhatsapp } from "react-icons/io";
-
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { Link } from "react-router-dom";
@@ -25,55 +20,62 @@ function Footer() {
           <div className="footerLogo">
             <img className="img" src={img} alt="" />
           </div>
-
           <p>Nossa missão é criar um lugar para as pessoas viverem em FÉ!</p>
           <div className="socials flex">
-            <a href="https://facebook.com" target="_blank" rel="noreferrer" >
+            <a href="https://facebook.com" target="_blank" rel="noreferrer">
               <ImFacebook className="icon" />
             </a>
-
             <a
               href="https://www.instagram.com/sjanchieta_?igsh=aWs5NjhjeXo5dWx4"
-              target="_blank" rel="noreferrer">
+              target="_blank"
+              rel="noreferrer"
+            >
               <AiFillInstagram className="icon" />
             </a>
-
             <a
               href="https://wa.me/556391399447"
-              target="_blank" rel="noreferrer">
+              target="_blank"
+              rel="noreferrer"
+            >
               <IoLogoWhatsapp className="icon" />
             </a>
-          </div>
-          <div className="footerReact">
-            <span>Feito com Amor</span>
           </div>
         </div>
 
         <div data-aos="fade-up" className="footerLinks">
           <span className="linkTitle">Menu</span>
-          
           <li>
-          <Link to="comunicados" className="link">
+            <Link to="comunidade" className="link">
+              Comunidade
+            </Link>
+          </li>
+          <li>
+            <Link to="comunicados" className="link">
               Comunicado
             </Link>
           </li>
           <li>
-          <Link to="clero" className="link">
+            <Link to="clero" className="link">
               Clero
             </Link>
           </li>
           <li>
-          <Link to="contatos" className="link">
+            <Link to="contatos" className="link">
               Contato
             </Link>
           </li>
           <li>
-          <Link to="login" className="link">
+            <Link to="login" className="link">
               Login
             </Link>
           </li>
-          
-         
+        </div>
+
+        <div className="footerReact">
+          <p>
+            Copyright Comunidade São José de Anchieta de Palmas © {new Date().getFullYear()}. Feito
+            com ❤️  
+          </p>
         </div>
       </div>
     </div>
